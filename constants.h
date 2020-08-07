fc239ce00b88878e60aea0af116b5b72a4f0d967
sysop1021 <mattwbrown@gmail.com>
2020-08-07 17:21:34 -0500

    bird2: speed adjustment

--- constants.h
+++ constants.h
@@ -4,8 +4,8 @@
 const int WINDOW_WIDTH  = 1280;
 const int WINDOW_HEIGHT = 720;
 const float SCALING_FACTOR = 2.5f;
-const float BACKGROUND_SCROLL_SPEED = 30.f;
-const float FOREGROUND_SCROLL_SPEED = 60.f;
+const float BACKGROUND_SCROLL_SPEED = 75.f;
+const float FOREGROUND_SCROLL_SPEED = 150.f;
 const int BACKGROUND_LOOP_POINT = 1032;
 
 #endif
